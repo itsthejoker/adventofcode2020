0ace5f2bb7c7ca707388bafb06c4666045a3a23f
Joe Kaufeld <joe@kenzie.academy>
2020-12-05 13:48:26 -0500

    add day 5
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

---
+++ adventofcode/day_05.py
@@ -0,0 +1,37 @@
+from data import load_data
+
+input = load_data()
+input = input.split("\n")
+input = [i for i in input if i != ""]
+
+
+def get_seat_id(row, col):
+    return row * 8 + col
+
+
+def chop(half, rows):
+    if half == 'F' or half == "L":
+        return rows[:int((len(rows) - 1) / 2 + 1)]
+    if half == 'B' or half == "R":
+        return rows[int((len(rows) - 1) / 2 + 1):]
+
+
+all_seat_ids = set()
+
+for seat in input:
+    rows = [i for i in range(128)]
+    cols = [i for i in range(8)]
+    row = seat[:7]
+    col = seat[7:]
+    for directive in row:
+        rows = chop(directive, rows)
+    row = rows[0]
+    for directive in col:
+        cols = chop(directive, cols)
+    col = cols[0]
+    seat_id = get_seat_id(row, col)
+    all_seat_ids.update({seat_id})
+
+for seat in range(80, 919):
+    if seat not in all_seat_ids:
+        print(seat)
