ca0353088aa45e882fad562304fefa2e20d19c2a
Joe Kaufeld <joe@kenzie.academy>
2020-12-09 01:02:42 -0500

    working
    Signed-off-by: Joe Kaufeld <joe@kenzie.academy>

--- aoc/day_09.py
+++ aoc/day_09.py
@@ -9,11 +9,11 @@ preamble_length = 25
 for count, num in enumerate(input):
     if count < preamble_length:
         continue
-    # x[26-25:26]
     result = any([a + b == num for a in input[count-preamble_length: count] for b in input[count-preamble_length: count]])
     if not result:
+        print("Found the number that doesn't belong")
         print(f"{count=}, {num=}")
         for window in range(600):
             for c in range(len(input)):
                 if sum(input[c:c+window]) == num:
-                    print("Possible answer: ", input[c:c+window][0]+input[c:c+window][-1])
+                    print("Possible answer: ", min(input[c:c+window])+max(input[c:c+window]))
