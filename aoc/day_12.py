0c87a97b3738e1a3fde1e03647c76f13e613b9cf
Joe Kaufeld <joe@kenzie.academy>
2020-12-12 19:41:48 -0500

    part 1 complete

--- aoc/day_12.py
+++ aoc/day_12.py
@@ -2,34 +2,55 @@ from data import load_data
 
 input = load_data()
 
-input = [i for i in input if i != '']
-
+input = [i for i in input.split('\n') if i != '']
 CURRENT_DIRECTION = "E"
 L_DIRECTIONS = ["E", "N", "W", "S"]
 R_DIRECTIONS = ["E", "S", "W", "N"]
 
+# [n/s, w/e]
 CURRENT_LOCATION = [0, 0]
+WAYPOINT_LOCATION = [1, 10]
 
-def turn(directive, amount):
-    steps = (amount / 90) % 4
+def turn(current_dir: str, directive: str, amount: int) -> str:
+    steps = int((amount / 90) % 4)
     if directive == "L":
         directions = L_DIRECTIONS
-    if directive == "R":
+    elif directive == "R":
         directions = R_DIRECTIONS
-    directions = (directions+directions)[
-        directions.index(CURRENT_DIRECTION):directions.index(CURRENT_DIRECTION)+4
+    window_of_directions = (directions+directions)[
+        directions.index(current_dir):directions.index(current_dir)+4
     ]
-    CURRENT_DIRECTION = directions[steps]
+    return window_of_directions[steps]
 
 
-def move(directive, amount):
-    pass
+def move(current_loc, directive, amount):
+    if directive == "N":
+        current_loc[0] += amount
+    elif directive == "S":
+        current_loc[0] -= amount
+    elif directive == "E":
+        current_loc[1] += amount
+    elif directive == "W":
+        current_loc[1] -= amount
+    elif directive == "F":
+        if CURRENT_DIRECTION == "N":
+            current_loc[0] += amount
+        elif CURRENT_DIRECTION == "S":
+            current_loc[0] -= amount
+        elif CURRENT_DIRECTION == "E":
+            current_loc[1] += amount
+        elif CURRENT_DIRECTION == "W":
+            current_loc[1] -= amount
+    return current_loc
 
 
 for instruction in input:
-    directive = instruction[:1].lower()
+    directive = instruction[:1].upper()
     amount = int(instruction[1:])
-    if "l" in directive or "r" in directive:
-        turn(directive, amount)
+    if "L" in directive or "R" in directive:
+        CURRENT_DIRECTION = turn(CURRENT_DIRECTION, directive, amount)
     else:
-        move(directive, amount)
+        CURRENT_LOCATION = move(CURRENT_LOCATION, directive, amount)
+
+print(abs(CURRENT_LOCATION[0]), abs(CURRENT_LOCATION[1]))
+print("--> ", abs(CURRENT_LOCATION[0]) + abs(CURRENT_LOCATION[1]))
